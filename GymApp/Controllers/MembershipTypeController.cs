efac16b75aaf38c6fa5135984fe0f50737256efa
webd9v <75453343+webd9v@users.noreply.github.com>
2022-07-16 19:43:19 -0700

    Final Version

--- GymApp/Controllers/MembershipTypeController.cs
+++ GymApp/Controllers/MembershipTypeController.cs
@@ -12,7 +12,8 @@ namespace GymApp.Controllers
     public class MembershipTypeController : Controller
     {
         private readonly ApplicationDbContext _db;
-
+        string duration;
+        string searchPhrase;
         public MembershipTypeController(ApplicationDbContext db)
         {
             _db = db;
@@ -21,12 +22,47 @@ namespace GymApp.Controllers
         public IActionResult Index()
         {
             IEnumerable<MembershipTypeModel> objMembershipsList = _db.MembershipTypes;
+            ViewBag.Duration = this.duration;
+            ViewBag.SearchPhrase = this.searchPhrase;
             return View(objMembershipsList);
         }
         
-        public async Task<IActionResult> ShowSearchResult(String SearchPhrase)
+        public async Task<IActionResult> ShowSearchResult(String SearchPhrase, String durationFilter)
         {
-            return View("Index", await _db.MembershipTypes.Where(j => j.Name.Contains(SearchPhrase)).ToListAsync());
+            var membershipTypes=  _db.MembershipTypes;
+            if (!(string.IsNullOrEmpty(SearchPhrase)) && !(string.IsNullOrEmpty(durationFilter)))
+            {
+
+                this.searchPhrase = SearchPhrase;
+                this.duration = durationFilter;
+
+                if (durationFilter.Equals("All"))
+                {
+                    return View("Index", await membershipTypes.Where(j => j.Name.Contains(SearchPhrase)).ToListAsync());
+
+                }
+                return View("Index", await membershipTypes.Where(mmtype=>mmtype.Name.Contains(SearchPhrase) && mmtype.Duration.Contains(durationFilter)).ToListAsync());
+
+            }
+            else if (!(string.IsNullOrEmpty(SearchPhrase)))
+            {
+                this.searchPhrase = SearchPhrase;
+
+                return View("Index", await membershipTypes.Where(j => j.Name.Contains(SearchPhrase)).ToListAsync());
+
+            }else if (!(string.IsNullOrEmpty(durationFilter)))
+            {
+                this.duration = durationFilter;
+
+                if (durationFilter.Equals("All"))
+                {
+                    return View("Index", await membershipTypes.ToListAsync());
+                }
+                return View("Index", await membershipTypes.Where(j => j.Duration.Contains(durationFilter)).ToListAsync());
+
+            }
+
+            return View("Index", await membershipTypes.ToListAsync());
         }
 
     }
